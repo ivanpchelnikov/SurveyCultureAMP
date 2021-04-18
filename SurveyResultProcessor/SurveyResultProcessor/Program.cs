a75127ed6a5574237c276daa2917ae38bc8ba44d
IvanP <ivan.pchelnikov@gmail.com>
2021-04-18 15:38:30 +1000

    Updated output.

--- SurveyResultProcessor/SurveyResultProcessor/Program.cs
+++ SurveyResultProcessor/SurveyResultProcessor/Program.cs
@@ -13,11 +13,18 @@ namespace SurveyResultProcessor
                 var statistic = new ResponseStatisticService().GetAverageForRatingQuestions(survey, surveyResponses);
 
                 Console.WriteLine("\nSurvey reult:\n");
-                Console.WriteLine($"Participation percentage {participants} and total participant counts of the survey: {percentage.ToString("P02")} \n");
+                Console.WriteLine($"Participation percentage {percentage.ToString("P02")} and total participant counts of the survey:  {participants}  \n");
                 Console.WriteLine($"The average for each rating question:\n");
-                foreach (var question in statistic)
+                if (statistic.Count == 0)
                 {
-                    Console.WriteLine($"Question: {question.Key} Average: {question.Value}");
+                    Console.WriteLine($"There is no submitted responses for this survey.");
+                }
+                else
+                {
+                    foreach (var question in statistic)
+                    {
+                        Console.WriteLine($"Question: {question.Key} Average: {question.Value}");
+                    }
                 }
             }
             catch (Exception ex)
